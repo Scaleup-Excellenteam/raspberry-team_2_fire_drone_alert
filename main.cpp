25b287bc02cd6ea5a98bc15c4c715748d9bfe355
Ori Vered <76990617+OriVered@users.noreply.github.com>
2023-08-29 11:30:01 +0300

    Update main.cpp
    Consts added

--- main.cpp
+++ main.cpp
@@ -12,6 +12,20 @@
 using namespace cv;
 using namespace std;
 
+const int TIMEOUT = 10;
+const int FRAME_WIDTH = 640;
+const int FRAME_HEIGHT = 480;
+const int RESIZED_WIDTH = 960;
+const int RESIZED_HEIGHT = 540;
+const Scalar LOWER_MASK(0, 120, 120);
+const Scalar UPPER_MASK(20, 255, 255);
+const std::string IP_INFO_URL = "http://ipinfo.io/json";
+const std::string POST_URL = "https://9e75-82-80-173-170.ngrok-free.app/message";
+const int FIRE_THRESHOLD = 15000;
+const Size BLUR_SIZE(21, 21);
+const double FPS = 10.0;
+const int MICROSECONDS_IN_SECOND = 1000000;
+
 CURL *curl;
 CURLcode res;
 
@@ -21,49 +35,29 @@ size_t Callback(const char* in, size_t size, size_t num, std::string* out){
     return totalBytes;
 }
 
-
 int getLocation(string &data){
-    const std::string url("http://ipinfo.io/json");
     CURL* curl = curl_easy_init();
-    
-    // Set remote URL.
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    
-    // Don't bother trying IPv6, which would increase DNS resolution time.
+    curl_easy_setopt(curl, CURLOPT_URL, IP_INFO_URL.c_str());
     curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-    
-    // Don't wait forever, time out after 10 seconds.
-    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
-    
-    // Follow HTTP redirects if necessary.
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, TIMEOUT);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
-    // Response information.
     int httpCode(0);
     std::unique_ptr<std::string> httpData(new std::string());
-
-    // Hook up data handling function.
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Callback);
-
-    // Hook up data container (will be passed as the last parameter to the
-    // callback handling function).  Can be any pointer type, since it will
-    // internally be passed as a void pointer.
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
-
-    // Run our HTTP GET command, capture the HTTP response code, and clean up.
     curl_easy_perform(curl);
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
     curl_easy_cleanup(curl);
 
-    if (!(httpCode == 200))
+    if (httpCode != 200)
     {
-        std::cerr << "Couldn't GET from " << url << " - exiting" << std::endl;
+        std::cerr << "Couldn't GET from " << IP_INFO_URL << " - exiting" << std::endl;
         return 1;
     }
 
     data = httpData.get()->c_str();
     return 0;
-
 }
 
 int sendMessage(const string& data){
@@ -74,18 +68,16 @@ int sendMessage(const string& data){
         headers = curl_slist_append(headers, "Accept: application/json");
         headers = curl_slist_append(headers, "Content-Type: application/json");
         headers = curl_slist_append(headers, "charset: utf-8");
-        curl_easy_setopt(curl, CURLOPT_URL, "https://9e75-82-80-173-170.ngrok-free.app/message");
+        curl_easy_setopt(curl, CURLOPT_URL, POST_URL.c_str());
         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcrp/0.1");
         res = curl_easy_perform(curl);
         if(res != CURLE_OK){
-        fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                curl_easy_strerror(res));
-                return 1;
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+            return 1;
         }
-        /* always cleanup */
         curl_easy_cleanup(curl);
     }
     curl_global_cleanup();
@@ -97,50 +89,42 @@ int FIRE_REPORTED = 0;
 int main(int, char **)
 {
     string data;
-    
     Mat frame, resized, blur, hsv, mask, output;
-    //--- INITIALIZE VIDEOCAPTURE
+
     VideoCapture cap;
     cap.open(0);
 
-    // check if we succeeded
     if (!cap.isOpened())
     {
         cerr << "ERROR! Unable to open camera\n";
         return -1;
     }
 
-    double fps = 10;
-
     namedWindow("drone-fire-detector", WINDOW_NORMAL);
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, FRAME_WIDTH); 
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
 
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640); 
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
     for (;;)
     {
         double start_time = (double)getTickCount();
-
-        // wait for a new frame from camera and store it into 'frame'
         cap.read(frame);
-
         double end_time = (double)getTickCount();
-        // check if we succeeded
+
         if (frame.empty())
         {
             cerr << "ERROR! blank frame grabbed\n";
             break;
         }
-        
-        resize(frame, resized, Size(960, 540));
-        GaussianBlur(resized, blur, Size(21, 21), 0);
-        cvtColor(blur, hsv, cv::COLOR_BGR2HSV);
 
-        Scalar lower(0,120,120), upper(20,255,255);
-        inRange(hsv, lower, upper, mask);
+        resize(frame, resized, Size(RESIZED_WIDTH, RESIZED_HEIGHT));
+        GaussianBlur(resized, blur, BLUR_SIZE, 0);
+        cvtColor(blur, hsv, cv::COLOR_BGR2HSV);
+        inRange(hsv, LOWER_MASK, UPPER_MASK, mask);
         bitwise_and(resized, hsv, output, mask);
         int no_red = countNonZero(mask);
 
-        if(no_red > 15000){
+        if(no_red > FIRE_THRESHOLD)
+        {
             ++FIRE_REPORTED;
             getLocation(data);
             cout << data;
@@ -152,13 +136,10 @@ int main(int, char **)
             break;
 
         double process_time = (end_time - start_time) / cv::getTickFrequency(); 
-        
-        if (process_time < 1. / fps) {
-            usleep(((1. / fps) - process_time)*1000000);
+        if (process_time < 1. / FPS)
+        {
+            usleep(((1. / FPS) - process_time) * MICROSECONDS_IN_SECOND);
         }
-
     }
-    
     return 0;
 }
-
